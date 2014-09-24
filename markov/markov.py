5ce74f8bc2bb9211da5a207a9ed0ba766eff542a
Evan Dorsky <evan.dorsky@icloud.com>
2014-09-24 18:22:21 -0400

    Getting some pretty good output now

--- markov/markov.py
+++ markov/markov.py
@@ -7,29 +7,33 @@ with open('seed.txt', 'r') as seed:
 	for line in seed:
 		words += line
 
-wordlist = re.findall(r"[\w']+", words)
+wordlist = re.findall(r"[\w']+[\".,;:]?", words)
 
 worddict = dict()
 
-prev = ''
-for word in wordlist:
-	if prev is '':
-		prev = word
-		continue
-
+prev = (wordlist[0], wordlist[1])
+for i in range(2, len(wordlist)):
+	word = wordlist[i]
 	if prev in worddict:
 		worddict[prev].append(word)
 	else:
 		worddict[prev] = [word]
 
-	prev = word
+	prev = (prev[1], word)
 
-word = random.choice(list(worddict.keys()))
+prefix = 'a'
+while not prefix[0].isupper():
+	prefix = random.choice(list(worddict.keys()))
 
-sentence = word
+i = 0
+sentence = prefix[0] + ' ' + prefix[1]
 while True:
-	if word not in list(worddict.keys()): break
-	word = random.choice(worddict[word])
-	sentence += ' ' + word
+	if prefix not in list(worddict.keys()):
+		sentence += '.'
+		break
+	if '.' in prefix[1]: break
+	prefix = (prefix[1], random.choice(worddict[prefix]))
+	sentence += ' ' + prefix[1]
+	i += 1
 
 print(sentence)
